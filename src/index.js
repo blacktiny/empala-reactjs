29e376203e3413cade53ad4f9f99264f2af95fb5
blacktiny <blacktiny126@hotmail.com>
2018-12-07 04:20:57 -0800

    update fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './common/index.css';
 import App from './views/App/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
